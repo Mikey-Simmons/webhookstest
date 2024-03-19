f15d6d8067ebb84d44877cf142cd8dca71262e07
Michael Simmons <msimmons@referralrock.com>
2024-03-19 15:24:56 -0700

    RR

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.post('/', (req, res) => {
     
 
     // Get the JSON body raw data
-    var payload = req
+    var payload = JSON.stringify(req)
 
     // Concatenate payload JSON body raw data with webhook event to match 
     // the content that was used before encrypting the signature.
